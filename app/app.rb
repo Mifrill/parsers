440709f5a24f03d2a50b2c074ecc17f73ce2ccd0
Mifrill <alexei.mifrill.strizhak@gmail.com>
2017-06-06 22:01:39 +0500

    remove sleeps

--- app/app.rb
+++ app/app.rb
@@ -7,7 +7,6 @@ module DNSParser
       page.driver.browser.manage.window.resize_to(1280, 1024)
 
       def get_price
-        sleep(0.5)
         find("input[class='form-control ui-autocomplete-input'").set("5\" Смартфон Huawei Honor 5A 16 ГБ золотистый").native.send_keys(:enter)
         price = find(".price_g span:first-child").text
         puts price
@@ -28,7 +27,6 @@ module DNSParser
         @regions_of_federal_regions = []
         find("ul[class='regions']").all('li').drop(1).each do |region|
           @regions_of_federal_regions << region.find('a')['data-region-id']
-          sleep(0.5)
         end
 
         @regions_of_federal_regions.each do |region|
@@ -37,17 +35,12 @@ module DNSParser
           @city_of_regions_of_federal_regions = []
           find("ul[class='cities']").all('li').drop(1).each do |city|
             @city_of_regions_of_federal_regions << city.find('a')['rel']
-            sleep(0.5)
           end
 
           @city_of_regions_of_federal_regions.each do |city|
-            sleep(0.5)
             find("ul[class='cities']").find("li a[rel='#{city}']").click
-            sleep(0.5)
             get_price
-            sleep(0.5)
             find("a[class='city-select w-choose-city-widget']").click
-            sleep(0.5)
           end
         end
       end
