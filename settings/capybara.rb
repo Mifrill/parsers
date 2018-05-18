36be2b8bb6f2af6e0e6d68f01b2a4ca89ef76b45
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2018-05-18 12:26:33 +0500

    chrome options by Capabilities

--- settings/capybara.rb
+++ settings/capybara.rb
@@ -3,9 +3,22 @@ require 'capybara-screenshot'
 require 'selenium-webdriver'
 require 'capybara/dsl'
 
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--remote-debugging-port=9222')
+
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+chrome_options = chrome_bin ? { 'chromeOptions' => { 'binary' => chrome_bin } } : options.as_json
+
 Capybara.run_server = false
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_options)
+  )
 end
 Capybara.default_driver = :chrome
 Capybara::Screenshot.register_driver(:chrome) do |driver, path|
