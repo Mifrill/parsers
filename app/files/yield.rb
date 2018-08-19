58956937854906fbf7b4a48e26cdaa610fc015da
Aleksey <alexei.mifrill.strizhak@gmail.com>
2018-08-19 12:48:06 +0500

    Implicit blocks are much faster than explicit one

--- app/files/yield.rb
+++ app/files/yield.rb
@@ -10,3 +10,33 @@ show_numbers do |number|
   puts "the numbers is #{number}"
   number *= 2 # does not matter
 end
+
+def explicit(&block)
+  raise LocalJumpError, "No block given." unless block
+  block.call
+end
+
+explicit do
+  puts "From explicit block"
+end
+
+begin
+  explicit
+rescue LocalJumpError => e
+  puts "#{e.class} #{e.message}"
+end
+
+def implicit
+  raise LocalJumpError, "No block given." unless block_given?
+  yield
+end
+
+implicit do
+  puts "From Implicit block"
+end
+
+begin
+  implicit
+rescue LocalJumpError => e
+  puts "#{e.class} #{e.message}"
+end
