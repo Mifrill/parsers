768f6be4e77cc621eb4055a2450e4d5a2f1fd5ff
Aleksey <alexei.mifrill.strizhak@gmail.com>
2018-08-21 23:17:44 +0500

    how the retry keyword works

--- app/files/base_command.rb
+++ app/files/base_command.rb
@@ -13,3 +13,14 @@ class BaseCommand
     raise 'Method "unexecute" must be implemented!'
   end
 end
+
+attempts = 0
+begin
+  make_service_call()
+rescue Exception
+  attempts += 1
+  retry unless attempts > 2
+  exit 1
+ensure
+  puts "ensure! #{attempts}"
+end
