85a015fd68403318add3b639ee2073e2b92a06a4
Aleksey <alexei.mifrill.strizhak@gmail.com>
2018-10-15 17:56:39 +0500

    add output suppressor

---
+++ practice/suppressor.rb
@@ -0,0 +1,29 @@
+module OutputSuppressor
+  def suppress_output
+    original_stdout, original_stderr = $stdout.clone, $stderr.clone
+    $stderr.reopen File.new("~", "w")
+    $stdout.reopen File.new("~", "w")
+    yield if block_given?
+  ensure
+    $stdout.reopen original_stdout
+    $stderr.reopen original_stderr
+  end
+end
+
+class Test
+  include OutputSuppressor
+
+  OUTPUT = 123
+
+  def test
+    block =-> { OUTPUT }
+
+    puts block.call
+
+    suppress_output do
+      puts "this output should be suppressed #{block.call}"
+    end
+  end
+end
+
+Test.new.test
