6cc8695bb9127958490fa915021786755f4b73e9
Aleksey <alexei.mifrill.strizhak@gmail.com>
2018-10-17 20:52:03 +0500

    practice: build custom Queue class

--- practice/mutex.rb
+++ practice/mutex.rb
@@ -67,24 +67,3 @@ class BankAccount
     end
   end
 end
-
-
-queue = Queue.new
-
-producer = Thread.new do
-  5.times do |i|
-    sleep rand(i) # simulate expense
-    queue << i
-    puts "#{i} produced"
-  end
-end
-
-consumer = Thread.new do
-  5.times do |i|
-    value = queue.pop
-    sleep rand(i/2) # simulate expense
-    puts "consumed #{value}"
-  end
-end
-
-[producer, consumer].map(&:join)
