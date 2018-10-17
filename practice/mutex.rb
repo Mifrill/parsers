b5a6e78de0fa6f687dd0a5f8c9491933c6058e56
Aleksey <alexei.mifrill.strizhak@gmail.com>
2018-10-17 18:37:08 +0500

    practice: Mutex use synchronize for reading operations too

--- practice/mutex.rb
+++ practice/mutex.rb
@@ -28,3 +28,20 @@ puts counters_with_mutex
 puts counters_without_mutex
 # => [500000, 500000, 217716, 355114, 258086, 500000, 442370, 500000, 500000, 500000]
 # note that we seem to have lost some increments here due to not using a mutex
+
+mutex = Mutex.new
+flags = [false, false, false, false, false, false, false, false, false, false]
+threads = 50.times.map do
+  Thread.new do
+    100000.times do
+      # don't do this! Reading from shared state requires a mutex!
+      puts flags.to_s
+      mutex.synchronize do
+        flags.map! { |f| !f }
+      end
+    end
+  end
+end
+threads.each(&:join)
+# ruby practice/mutex.rb > output.log
+# grep 'true, false' output.log | wc -l
