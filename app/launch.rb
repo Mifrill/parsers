6d2efc0db744e196ae0ebe616d19ec496da748d6
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2018-10-24 17:37:56 +0500

    remove DNS parser

--- app/launch.rb
+++ app/launch.rb
@@ -1,6 +1,6 @@
 require_relative 'app'
 
 parser_name = ARGV.shift
-abort 'specify the parser name (for example: DNS)' unless parser_name
+abort 'specify the parser name (for example: Test)' unless parser_name
 
 Parser.build(parser_name).new
